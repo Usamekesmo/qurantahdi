88dc43171693cb7474f7082ae3da069e0061884f
qurantests <91206259+Usamekesmo@users.noreply.github.com>
2025-08-20 15:52:26 +0300

    Add files via upload

---
+++ js/domElements.js
@@ -0,0 +1,23 @@
+// js/domElements.js
+
+export const settingsScreen = document.getElementById('settings-screen');
+export const loadingScreen = document.getElementById('loading-screen');
+export const quizScreen = document.getElementById('quiz-screen');
+export const resultsScreen = document.getElementById('results-screen');
+export const startTestBtn = document.getElementById('start-test-btn');
+export const errorMessage = document.getElementById('error-message');
+export const actionButtonsContainer = document.getElementById('action-buttons-container');
+export const reciterSelect = document.getElementById('reciter-select');
+export const surahSelect = document.getElementById('surah-select');
+export const startPageInput = document.getElementById('start-page');
+export const endPageInput = document.getElementById('end-page');
+export const highScoreDisplay = document.getElementById('high-score-display');
+export const userNameInput = document.getElementById('user-name');
+export const numQuestionsInput = document.getElementById('num-questions');
+export const userNameDisplay = document.getElementById('user-name-display');
+export const questionCounter = document.getElementById('question-counter');
+export const questionTitle = document.getElementById('question-title');
+export const questionArea = document.getElementById('question-area');
+export const optionsContainer = document.getElementById('options-container');
+export const finalUserName = document.getElementById('final-user-name');
+export const finalScore = document.getElementById('final-score');
